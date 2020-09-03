3fe5a8819bb934fc756e69225e9bd57bf834facf
Patrick Smith <pgwsmith@gmail.com>
2020-09-03 21:08:39 +1000

    Working at Elevate Partners

--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -20,13 +20,13 @@ function LinkItem({
 function Home() {
 	return (
 		<main class={style.home}>
-				<h1>Patrick George Wyndham Smith · Developer & Designer</h1>
+				<h1>{'Patrick George Wyndham Smith · Developer & Designer'}</h1>
 			<Block>
-				<p>I am a digital product developer & designer from Melbourne, Australia, focused on creating apps for passionate professionals, both for clients and through my own projects.</p>
+				<p>{'I am a digital product developer & designer from Melbourne, Australia, focused on creating apps for passionate professionals, both for clients and through my own projects.'}</p>
 
-				<p>Completing a Bachelor of Multimedia Design at Swinburne University in 2009, I then freelanced for companies large & small. In 2011 I co-founded adventure sports video creator and curator InfinityList, where I was the primary developer, and worked on UI/UX design, strategy & monetisation. From 2016 to beginning of 2018, I was lead educator at Coder Academy where I taught 3 six-month bootcamps.</p>
+				<p>{'Completing a Bachelor of Multimedia Design at Swinburne University in 2009, I then freelanced for companies large & small. In 2011 I co-founded adventure sports video creator and curator InfinityList, where I was the primary developer, and worked on UI/UX design, strategy & monetisation. From 2016 to beginning of 2018, I was lead educator at Coder Academy where I taught 3 six-month bootcamps.'}</p>
 
-				<p>I now work at <a href="https://cogent.co/">Cogent</a>, a digital product consultancy. I also lead a range of open source projects, using Golang, Elm, JavaScript & Swift, under the organisation <a href="https://github.com/RoyalIcing">@RoyalIcing</a>.</p>
+				<p>I now work at <a href="https://www.elevatepartners.com.au/">Elevate Partners</a>, a digital product consultancy. I also lead a range of open source projects, using Golang, Elixir, TypeScript & Swift, under the organisation <a href="https://github.com/RoyalIcing">@RoyalIcing</a>.</p>
 
 				<nav class={style.linksNav}>
 					<ul class={style.links}>
